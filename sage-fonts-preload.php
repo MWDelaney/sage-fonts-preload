eadeda917faf473f516693e1a201e30d476f891d
MWDelaney <michael.delaney@gmail.com>
2021-01-13 13:46:18 -0500

    :tada: First version!

---
+++ sage-fonts-preload.php
@@ -0,0 +1,14 @@
+<?php
+use Illuminate\Support\Str;
+
+use function Roots\asset;
+
+add_filter('wp_head', function () {
+    echo collect(
+        json_decode(file_get_contents(asset('mix-manifest.json')))
+    )->keys()->filter(function ($item) {
+        return Str::endsWith($item, ['.otf', '.eot', 'woff', 'woff2', 'ttf']);
+    })->map(function ($item) {
+        return sprintf('<link rel="preload" href="%s%s" as="font" crossorigin>', get_stylesheet_directory_uri() . '/dist', $item);
+    })->implode("\n");
+});
