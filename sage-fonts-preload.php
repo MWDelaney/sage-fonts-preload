489dd82039fab98cc118a199a3d77208e3c85e65
MWDelaney <michael.delaney@gmail.com>
2021-01-13 14:20:20 -0500

    :art: Better error checking

--- sage-fonts-preload.php
+++ sage-fonts-preload.php
@@ -7,11 +7,27 @@ if (! function_exists('add_filter')) {
     return;
 }
 
+if (! file_exists(asset('mix-manifest.json'))) {
+    return;
+}
+
 add_filter('wp_head', function () {
+    // Set an array of font types
+    $types = ['.otf', '.eot', 'woff', 'woff2', 'ttf'];
+
+    // Get the mix manifest contents
+    $json = json_decode(file_get_contents(asset('mix-manifest.json')));
+
+    // If the json is empty, return gracefully;
+    if ($json->isEmpty()) {
+        return;
+    }
+
+    // Echo link tags for each font
     echo collect(
-        json_decode(file_get_contents(asset('mix-manifest.json')))
+        $json
     )->keys()->filter(function ($item) {
-        return Str::endsWith($item, ['.otf', '.eot', 'woff', 'woff2', 'ttf']);
+        return Str::endsWith($item, $types);
     })->map(function ($item) {
         return sprintf('<link rel="preload" href="%s%s" as="font" crossorigin>', get_stylesheet_directory_uri() . '/dist', $item);
     })->implode("\n");
